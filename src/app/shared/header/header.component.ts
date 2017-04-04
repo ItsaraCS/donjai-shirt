33dc1fba9ce2be8653c7e57e5869b37cb8c3073d
ItsaraCS <itsara.ra.cs@hotmail.com>
2017-04-05 00:16:17 +0700

    05-04-17 [Ver.1]

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let $: any;
+
+$(document).ready(function() {
+  $(document).on('click', '.show-link-mobile', function(e) {
+    e.preventDefault();
+    
+    var showLinkMobileClass = '{ display: block }';
+    //$('.link-mobile').css({ 'display': 'block' });
+
+    if($('.link-mobile').is(':visible'))
+      $('.link-mobile').css({ 'display': 'none' });
+    else
+      $('.link-mobile').css({ 'display': 'block' });
+  });
+});
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
